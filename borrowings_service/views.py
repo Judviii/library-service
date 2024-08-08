72611c3f21554e6034e6ba657e866c2c57bf8e0e
Vadim Babii <judviiioff@gmail.com>
2024-08-08 18:11:06 +0300

    implement list & detail endpoints, configure URLs for borrowing service

--- borrowings_service/views.py
+++ borrowings_service/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from borrowings_service.models import Borrowing
+from borrowings_service.serializers import (
+    BorrowingSerializer, BorrowingDetailSerializer
+)
 
-# Create your views here.
+
+class BorrowingViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        return BorrowingSerializer

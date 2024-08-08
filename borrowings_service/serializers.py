bd530f5cc7ca5b7e6e70e8ae80042e036182a647
Vadim Babii <judviiioff@gmail.com>
2024-08-08 18:07:13 +0300

    implement list serializer and detail serializer with detailed book info

--- borrowings_service/serializers.py
+++ borrowings_service/serializers.py
@@ -1,1 +1,27 @@
 from rest_framework import serializers
+from books_service.models import Book
+from borrowings_service.models import Borrowing
+from books_service.serializers import BookSerializer
+
+
+class BookDetailSerializer(BookSerializer):
+    class Meta:
+        model = Book
+        fields = ("title", "author", "cover", "daily_fee")
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        )
+
+
+class BorrowingDetailSerializer(BorrowingSerializer):
+    book = BookDetailSerializer(many=False, read_only=True)

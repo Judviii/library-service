01ceeb724f3f0843792944e9d4f3c5c45e2fe2f6
Vadim Babii <judviiioff@gmail.com>
2024-08-08 03:02:43 +0300

    add permissions for BookViewSet actions

--- books_service/views.py
+++ books_service/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 
 from books_service.models import Book
 from books_service.serializers import BookSerializer, BookListSerializer
@@ -12,3 +13,14 @@ class BookViewSet(viewsets.ModelViewSet):
         if self.action == "list":
             return BookListSerializer
         return BookSerializer
+
+    def get_permissions(self):
+        if self.action in ["create", "destroy", "update", "partial_update"]:
+            permission_classes = [IsAdminUser, ]
+        elif self.action == "retrieve":
+            permission_classes = [IsAuthenticated, ]
+        else:
+            permission_classes = [AllowAny, ]
+
+        return [permission() for permission in permission_classes]
+

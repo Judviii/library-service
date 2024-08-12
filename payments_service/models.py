5fe427f1aa26aab733ae93d595492afac131423f
Vadim Babii <judviiioff@gmail.com>
2024-08-12 20:09:41 +0300

    change max_length in field session_url to 500, remove unnecessary code in serrializers.py, add stripe to requirements

--- payments_service/models.py
+++ payments_service/models.py
@@ -22,7 +22,7 @@ class Payment(models.Model):
         default=TypeChoices.PAYMENT,
     )
     borrowing_id = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
-    session_url = models.URLField(max_length=200)
+    session_url = models.URLField(max_length=500)
     session_id = models.CharField(max_length=255)
     money_to_pay = models.DecimalField(
         max_digits=20,

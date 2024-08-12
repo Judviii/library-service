397b19bbd432508d8785774eaa984d9a482aacc7
Vadim Babii <judviiioff@gmail.com>
2024-08-12 20:12:33 +0300

    implement create_checkout_session action to generate Stripe checkout session ID and URL

--- payments_service/views.py
+++ payments_service/views.py
@@ -1,8 +1,7 @@
 import stripe
-from rest_framework.generics import get_object_or_404
 
-from library_api import settings
-from django.shortcuts import redirect, get_object_or_404
+from django.conf import settings
+from django.http import JsonResponse
 from rest_framework import viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -33,3 +32,36 @@ class PaymentViewSet(
         if user.is_staff:
             return Payment.objects.all()
         return Payment.objects.filter(borrowing_id__user=user)
+
+    @action(
+        methods=["GET"],
+        detail=True,
+        url_path="create-checkout-session",
+        permission_classes=[IsAuthenticated],
+    )
+    def create_checkout_session(self, request, pk=None):
+        domain_url = "http://localhost:8000/api/payments/"
+        checkout_session = stripe.checkout.Session.create(
+            success_url=domain_url,
+            cancel_url=domain_url,
+            payment_method_types=["card"],
+            mode="payment",
+            line_items=[
+                {
+                    "price_data": {
+                        "currency": "usd",
+                        "product_data": {
+                            "name": "Payment for BOOK",
+                        },
+                        "unit_amount": 20000,
+                    },
+                    "quantity": 1,
+                }
+            ]
+        )
+        return JsonResponse(
+            {
+                "sessionId": checkout_session["id"],
+                "sessionUrl": checkout_session["url"]
+            }
+        )
